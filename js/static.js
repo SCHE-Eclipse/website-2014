1e17e356c29beb050b112bdfcad0fd87379d1e1f
Eric Collins <physbrain@gmail.com>
2015-08-31 17:11:50 -0500

    Comment out all Physijs mesh objects. Replace with equivalent THREE.Mesh objects.

--- js/static.js
+++ js/static.js
@@ -16,7 +16,8 @@ function initGround() {
     
     // Ground
     var groundPlane = new THREE.PlaneGeometry(270, 270);
-    var ground = new Physijs.BoxMesh( groundPlane, groundMat );
+    //var ground = new Physijs.BoxMesh( groundPlane, groundMat );
+    var ground = new THREE.Mesh( groundPlane, groundMat );
     ground.visible = true;
     ground.receiveShadow = true;
     scene.add( ground );
@@ -54,7 +55,8 @@ function initWalls(section) {
         var y1 = 135.75;
         var r1 = Math.sqrt(x1*x1 + y1*y1);
         var theta1 = Math.atan2(y1, x1);
-        var wallMesh1 = new Physijs.BoxMesh( wallGeom1, woodMat);
+        //var wallMesh1 = new Physijs.BoxMesh( wallGeom1, woodMat);
+        var wallMesh1 = new THREE.Mesh( wallGeom1, woodMat);
         wallMesh1.rotation.z = theta;
         wallMesh1.position.set( r1*Math.cos(theta + theta1),
                                 r1*Math.sin(theta + theta1),
@@ -67,7 +69,8 @@ function initWalls(section) {
         var y2 = 111.0;
         var r2 = Math.sqrt(x2*x2 + y2*y2);
         var theta2 = Math.atan2(y2, x2);
-        var wallMesh2 = new Physijs.BoxMesh( wallGeom2, woodMat );
+        //var wallMesh2 = new Physijs.BoxMesh( wallGeom2, woodMat );
+        var wallMesh2 = new THREE.Mesh( wallGeom2, woodMat );
         wallMesh2.rotation.z = theta;
         wallMesh2.position.set( r2*Math.cos(theta + theta2),
                                 r2*Math.sin(theta + theta2),
@@ -80,7 +83,8 @@ function initWalls(section) {
         var y3 = 56.0;
         var r3 = Math.sqrt(x3*x3 + y3*y3);
         var theta3 = Math.atan2(y3, x3);
-        var wallMesh3 = new Physijs.BoxMesh( wallGeom3, woodMat);
+        //var wallMesh3 = new Physijs.BoxMesh( wallGeom3, woodMat);
+        var wallMesh3 = new THREE.Mesh( wallGeom3, woodMat);
         wallMesh3.rotation.z = theta + Math.PI/4;
         wallMesh3.position.set( r3*Math.cos(theta + theta3),
                                 r3*Math.sin(theta + theta3),
@@ -95,21 +99,22 @@ function makeBadRoad(section) {
     var active = (section == activeSection);
 
     var roadGeom = new THREE.BoxGeometry(36, 36, 0.25);
+    //var roadMat = Physijs.createMaterial( roadMat, 0.8, 0.2 );
     var roadMat = new THREE.MeshLambertMaterial({color: 0x664422});
-    var roadMat = Physijs.createMaterial( roadMat, 0.8, 0.2 );
     var roadMesh = new THREE.Mesh( roadGeom, roadMat);
 
     roadMesh.position.z = 0.125;
     roadMesh.receiveShadow = true;
 
     var bumpGeom = new THREE.CylinderGeometry(0.5, 0.5, 8.0);
+    //var bumpMat = Physijs.createMaterial( bumpMat, 0.8, 0.2 );
     var bumpMat = new THREE.MeshLambertMaterial({color: 0x442200});
-    var bumpMat = Physijs.createMaterial( bumpMat, 0.8, 0.2 );
     var bump;
     var offset = 4.5*Math.cos(Math.PI/4)-17.5;
     for (var i=0; i<4; ++i) {
         for (var j=0; j<4; ++j) {
-            bump = new Physijs.BoxMesh(bumpGeom, bumpMat);
+            //bump = new Physijs.BoxMesh(bumpGeom, bumpMat);
+            bump = new THREE.Mesh(bumpGeom, bumpMat);
             bump.rotation.z = Math.PI/4;
             bump.position.set(offset+9.5*i, offset+9.5*j, 0.25);
             bump.receiveShadow = true;
@@ -119,8 +124,8 @@ function makeBadRoad(section) {
     offset += 4.75;
     for (var i=0; i<3; ++i) {
         for (var j=0; j<3; ++j) {
-            bump = new Physijs.BoxMesh(bumpGeom, bumpMat);
-	    
+            //bump = new Physijs.BoxMesh(bumpGeom, bumpMat);
+            bump = new THREE.Mesh(bumpGeom, bumpMat);
             bump.rotation.z = -Math.PI/4;
             bump.position.set(offset+9.5*i, offset+9.5*j, 0.25);
             bump.receiveShadow = true;
@@ -138,11 +143,16 @@ function makeBridge(section) {
     var uprightGeom = new THREE.BoxGeometry(0.75, 4, 36);
 
     var center, ramp1, ramp2, up1, up2;
-    center = new Physijs.BoxMesh(plankGeom, woodMat);
-    ramp1 = new Physijs.BoxMesh(plankGeom, woodMat);
-    ramp2 = new Physijs.BoxMesh(plankGeom, woodMat);
-    up1 = new Physijs.BoxMesh(uprightGeom, woodMat);
-    up2 = new Physijs.BoxMesh(uprightGeom, woodMat);
+    //center = new Physijs.BoxMesh(plankGeom, woodMat);
+    //ramp1 = new Physijs.BoxMesh(plankGeom, woodMat);
+    //ramp2 = new Physijs.BoxMesh(plankGeom, woodMat);
+    //up1 = new Physijs.BoxMesh(uprightGeom, woodMat);
+    //up2 = new Physijs.BoxMesh(uprightGeom, woodMat);
+    center = new THREE.Mesh(plankGeom, woodMat);
+    ramp1 = new THREE.Mesh(plankGeom, woodMat);
+    ramp2 = new THREE.Mesh(plankGeom, woodMat);
+    up1 = new THREE.Mesh(uprightGeom, woodMat);
+    up2 = new THREE.Mesh(uprightGeom, woodMat);
  
     center.castShadow = true;
     ramp1.castShadow = true;
@@ -184,12 +194,18 @@ function makeOSOW (section) {
     var buttonGeom = new THREE.BoxGeometry(1.5, 1.5, 6);
 
     var backPlane, base, foot1, foot2, arm, button;
-    backPlane = new Physijs.BoxMesh(backPlaneGeom, woodMat);
-    base = new Physijs.BoxMesh(baseGeom, woodMat);
-    foot1 = new Physijs.BoxMesh(footGeom, woodMat);
-    foot2 = new Physijs.BoxMesh(footGeom, woodMat);
-    arm = new Physijs.BoxMesh(armGeom, woodMat);
-    button = new Physijs.BoxMesh(buttonGeom, woodMat);
+    //backPlane = new Physijs.BoxMesh(backPlaneGeom, woodMat);
+    //base = new Physijs.BoxMesh(baseGeom, woodMat);
+    //foot1 = new Physijs.BoxMesh(footGeom, woodMat);
+    //foot2 = new Physijs.BoxMesh(footGeom, woodMat);
+    //arm = new Physijs.BoxMesh(armGeom, woodMat);
+    //button = new Physijs.BoxMesh(buttonGeom, woodMat);
+    backPlane = new THREE.Mesh(backPlaneGeom, woodMat);
+    base = new THREE.Mesh(baseGeom, woodMat);
+    foot1 = new THREE.Mesh(footGeom, woodMat);
+    foot2 = new THREE.Mesh(footGeom, woodMat);
+    arm = new THREE.Mesh(armGeom, woodMat);
+    button = new THREE.Mesh(buttonGeom, woodMat);
     
     backPlane.position.set(0, 0, 20);
     backPlane.castShadow = true;
